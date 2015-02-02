f8ac0287833aeaf42cad561d94cd8a77a0f30da1
Sebastian Boehm <sebastian@sometimesfood.org>
2015-02-02 13:56:00 +0100

    Move code to files in lib/fakeeetc

--- lib/fakeetc.rb
+++ lib/fakeetc.rb
@@ -1,30 +1,6 @@
+require 'fakeetc/base'
+require 'fakeetc/groups'
 require 'fakeetc/version'
 
 module FakeEtc
-  def self.add_groups(group_hash)
-    group_hash.each do |group_name, group_info|
-      group = Struct::Group.new(group_name,
-                                'x',
-                                group_info[:gid],
-                                group_info[:mem])
-      @groups ||= {}
-      @groups[group_name] = group
-    end
-  end
-
-  def self.clear_groups
-    @groups = {}
-  end
-
-  def self.getgrnam(group_name)
-    group = @groups[group_name]
-    fail ArgumentError, "can't find group for #{group_name}" if group.nil?
-    group
-  end
-
-  def self.getgrgid(gid)
-    group = @groups.values.find { |g| g.gid == gid }
-    fail ArgumentError, "can't find group for #{gid}" if group.nil?
-    group
-  end
 end
