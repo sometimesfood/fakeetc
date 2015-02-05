08c2260a8d8449be17625574ff29f4281c51b7d1
Sebastian Boehm <sebastian@sometimesfood.org>
2015-02-05 11:05:09 +0100

    Use "class << self" notation for users and groups

--- lib/fakeetc/groups.rb
+++ lib/fakeetc/groups.rb
@@ -1,46 +1,46 @@
 module FakeEtc
-  def self.add_groups(group_hash)
-    group_hash.each do |group_name, group_info|
-      group = Struct::Group.new(group_name,
-                                'x',
-                                group_info[:gid],
-                                group_info[:mem])
-      @groups[group_name] = group
-    end
-  end
-
-  def self.clear_groups
-    @groups = {}
-  end
-
-  def self.getgrnam(group_name)
-    group = @groups[group_name]
-    fail ArgumentError, "can't find group for #{group_name}" if group.nil?
-    group
-  end
-
-  def self.getgrgid(gid)
-    group = @groups.values.find { |g| g.gid == gid }
-    fail ArgumentError, "can't find group for #{gid}" if group.nil?
-    group
-  end
-
-  def self.getgrent
-    @grents ||= @groups.values
-    @grents.shift
-  end
-
-  def self.endgrent
-    @grents = nil
-  end
   class << self
-    alias_method :setgrent, :endgrent
-  end
+    def add_groups(group_hash)
+      group_hash.each do |group_name, group_info|
+        group = Struct::Group.new(group_name,
+                                  'x',
+                                  group_info[:gid],
+                                  group_info[:mem])
+        @groups[group_name] = group
+      end
+    end
 
-  def self.group
-    return getgrent unless block_given?
-    @groups.values.each { |g| yield g }
-    endgrent
-    nil
+    def clear_groups
+      @groups = {}
+    end
+
+    def getgrnam(group_name)
+      group = @groups[group_name]
+      fail ArgumentError, "can't find group for #{group_name}" if group.nil?
+      group
+    end
+
+    def getgrgid(gid)
+      group = @groups.values.find { |g| g.gid == gid }
+      fail ArgumentError, "can't find group for #{gid}" if group.nil?
+      group
+    end
+
+    def getgrent
+      @grents ||= @groups.values
+      @grents.shift
+    end
+
+    def endgrent
+      @grents = nil
+    end
+    alias_method :setgrent, :endgrent
+
+    def group
+      return getgrent unless block_given?
+      @groups.values.each { |g| yield g }
+      endgrent
+      nil
+    end
   end
 end
--- lib/fakeetc/users.rb
+++ lib/fakeetc/users.rb
@@ -1,49 +1,49 @@
 module FakeEtc
-  def self.add_users(user_hash)
-    user_hash.each do |user_name, user_info|
-      user = Struct::Passwd.new(user_name,
-                                'x',
-                                user_info[:uid],
-                                user_info[:gid],
-                                user_info[:gecos],
-                                user_info[:dir],
-                                user_info[:shell])
-      @users[user_name] = user
-    end
-  end
-
-  def self.clear_users
-    @users = {}
-  end
-
-  def self.getpwnam(user_name)
-    user = @users[user_name]
-    fail ArgumentError, "can't find user for #{user_name}" if user.nil?
-    user
-  end
-
-  def self.getpwuid(uid)
-    user = @users.values.find { |u| u.uid == uid }
-    fail ArgumentError, "can't find user for #{uid}" if user.nil?
-    user
-  end
-
-  def self.getpwent
-    @pwents ||= @users.values
-    @pwents.shift
-  end
-
-  def self.endpwent
-    @pwents = nil
-  end
   class << self
-    alias_method :setpwent, :endpwent
-  end
+    def add_users(user_hash)
+      user_hash.each do |user_name, user_info|
+        user = Struct::Passwd.new(user_name,
+                                  'x',
+                                  user_info[:uid],
+                                  user_info[:gid],
+                                  user_info[:gecos],
+                                  user_info[:dir],
+                                  user_info[:shell])
+        @users[user_name] = user
+      end
+    end
 
-  def self.passwd
-    return getpwent unless block_given?
-    @users.values.each { |u| yield u }
-    endpwent
-    nil
+    def clear_users
+      @users = {}
+    end
+
+    def getpwnam(user_name)
+      user = @users[user_name]
+      fail ArgumentError, "can't find user for #{user_name}" if user.nil?
+      user
+    end
+
+    def getpwuid(uid)
+      user = @users.values.find { |u| u.uid == uid }
+      fail ArgumentError, "can't find user for #{uid}" if user.nil?
+      user
+    end
+
+    def getpwent
+      @pwents ||= @users.values
+      @pwents.shift
+    end
+
+    def endpwent
+      @pwents = nil
+    end
+    alias_method :setpwent, :endpwent
+
+    def passwd
+      return getpwent unless block_given?
+      @users.values.each { |u| yield u }
+      endpwent
+      nil
+    end
   end
 end
